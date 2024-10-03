70038e4e89aa2796ab4e63e7e80b268015f3c512
Yilin Zheng <zylin0711@gmail.com>
2024-10-02 21:03:58 -0400

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,14 +5,15 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
+        if (n  == 1) {
             return 1;
-        } else if (n == 5) {
+        } else if (n % 2 == 1) {
             return 3 * n + 1;
         } else {
-            return n * 2;
+            return n / 2;
         }
     }
+
 
     public static void main(String[] args) {
         int n = 5;
